855a043caaee3c63f5a4316d1143e8e558a51d4b
gsousa12 <wgsousa12@gmail.com>
2024-11-03 19:38:08 -0300

    adiciona useStage que manipula o form

--- Apps/To Do List - React + Vite/components/TodoForm.jsx
+++ Apps/To Do List - React + Vite/components/TodoForm.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import { useState } from "react";
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value || !category) return;
+    console.log(value, category);
+    setValue("");
+    setCategory("");
+  };
   return (
     <div className="todo-form">
       <h2>Criar Tarefa</h2>
-      <form>
-        <input type="text" placeholder="Digite o Título" />
-        <select>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o Título"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+        <select onChange={(e) => setCategory(e.target.value)} value={category}>
           <option value="">Selecione uma categoria</option>
           <option value="Trabalho">Trabalho</option>
           <option value="Pessoal">Pessoal</option>
