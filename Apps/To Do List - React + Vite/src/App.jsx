144f2c4bac9269b67fe80ca21edb16a98f1acce2
gsousa12 <wgsousa12@gmail.com>
2024-11-03 18:32:14 -0300

    add formulario e estiliza butoes

--- Apps/To Do List - React + Vite/src/App.jsx
+++ Apps/To Do List - React + Vite/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Todo from "../components/Todo";
+import TodoForm from "../components/TodoForm";
 import "./App.css";
 
 function App() {
@@ -28,16 +30,10 @@ function App() {
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <div className="todo">
-            <div className="content">
-              <p>{todo.text}</p>
-              <p className="category">({todo.category})</p>
-            </div>
-            <button>Completar</button>
-            <button>X</button>
-          </div>
+          <Todo todo={todo} />
         ))}
       </div>
+      <TodoForm />
     </div>
   );
 }
