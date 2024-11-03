f996906c29f4708386ef78669f504aba13e38c06
gsousa12 <wgsousa12@gmail.com>
2024-11-03 18:05:21 -0300

    Add logica do useState do Todo e add estilização

--- Apps/To Do List - React + Vite/src/App.jsx
+++ Apps/To Do List - React + Vite/src/App.jsx
@@ -2,11 +2,42 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
 
   return (
-    <div>
-      <h1>To Do List</h1>
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <div className="todo">
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="category">({todo.category})</p>
+            </div>
+            <button>Completar</button>
+            <button>X</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
